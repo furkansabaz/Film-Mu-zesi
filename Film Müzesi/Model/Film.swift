2d8de72ec9f6614def7f275de774482b8b0c3af9
Furkan Sabaz <furkan.sabaz@gmail.com>
2019-04-29 15:08:31 +0300

    1. Ders

---
+++ Film Müzesi/Model/Film.swift
@@ -0,0 +1,18 @@
+//
+//  Film.swift
+//  Film Müzesi
+//
+//  Created by Furkan Sabaz on 29.04.2019.
+//  Copyright © 2019 Furkan Sabaz. All rights reserved.
+//
+
+import Foundation
+
+
+struct Film {
+    
+    var adi : String
+    var detaylari : String
+    var goruntuAdi : String
+    
+}
---
+++ Film Müzesi/Model/FilmKategori.swift
@@ -0,0 +1,16 @@
+//
+//  FilmKategori.swift
+//  Film Müzesi
+//
+//  Created by Furkan Sabaz on 29.04.2019.
+//  Copyright © 2019 Furkan Sabaz. All rights reserved.
+//
+
+import Foundation
+
+
+struct  FilmKategori {
+    
+    let kategoriAdi : String
+    let goruntuAdi : String
+}
